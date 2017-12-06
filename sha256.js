0c91617cee05c6f21c46ad0cf7e3ef6cd410b8f0
Pablo Mangione <Mangione.mkt@gmail.com>
2017-12-06 19:44:21 +0100

    Delete sha256.js

--- sha256.js
+++
@@ -1,8 +0,0 @@
-// timestamp for the signature
-let timestamp = +new Date()/1000
-// public key from btc average
-const public_key = process.env.PUBLICKEY_BTCAVERAGE
-// secret from btc average
-const secret = process.env.SECRET_BTCAVERAGE
-// value needed to make the call (timestamp+"."+public_key signed with the secret)
-let digest_value = crypto.createHmac('SHA256', secret).update(`${timestamp}.${public_key}`).digest('hex').toString('base64')
