396b571131323d35180e853bc38fc82cac412f53
Pablo Mangione <mangione.mkt@gmail.com>
2017-12-06 23:22:12 +0100

    added populate method

--- server/utils/PostsDBService.js
+++ server/utils/PostsDBService.js
@@ -5,6 +5,9 @@ const PostsDBService = {}
 PostsDBService.listPosts = () => {
 	return new Promise((resolve,reject) => {
 		Posts.find()
+			.populate({
+				select:'title text link'
+			})
 			.then(posts => {
 				resolve(posts)
 			})
